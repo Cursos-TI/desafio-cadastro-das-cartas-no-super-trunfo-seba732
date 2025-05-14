a6dc6567fbecb7d9f36c7b00f2f5fbcd0954fe34
seba732 <sebajcp@gmail.com>
2025-05-14 20:20:59 +0000

    commit3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,17 @@ int main() {
     char estado;
     char codigo1, codigo2; //tive dificuldade pesquisei um ponco e foi uma forma mais facil que entendi
     char cidade [25];  
-    float area = 7000.000;
+    float area = 7000.000; 
     float pib = 456.0000;
     int pontoturistico = 20;
+
+    //Agora vou declarar as variaveis da carta 2
+    char estado2;
+    char codigo3, coddigo4;
+    char cidade2 [24];
+    float area2 = 70.000000;
+    float pib2 = 56.000000;
+    int pontoturistico2 = 20;
 
     //aqui peço para digitar os dados da carta 1
     printf("Digite o codigo do estado de A-H: ");
@@ -28,21 +36,7 @@ int main() {
     printf("Digete o numero de pontos turisticos: ");
     scanf("%d", &pontoturistico);
 
-    //aqui eu faço print das respostas da carta1 
-    printf("O estado é: %c\n", estado);
-    printf("O codigo da carta é: %c%c\n", codigo1, codigo2);
-    printf("O nome da cidade é: %s\n", cidade);
-    printf("A area da cidade é: %f\n", area);
-    printf("O pibe da cidade é: %f\n", pib);
-    printf("O numero de pontostos turisticos é: %d\n", pontoturistico);
-
-    //Agora vou declarar as variaveis da carta 2
-    char estado2;
-    char codigo3, coddigo4;
-    char cidade2 [24];
-    float area2 = 70.000000;
-    float pib2 = 56.000000;
-    int pontoturistico2 = 20;
+      
 
     //peço para digitar os dados da carta 2, para diferenciar as cartas peço que digite valores diferentes da primeira 
     printf("Digite o estado da carta 2 de A-H ");
@@ -62,6 +56,14 @@ int main() {
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontoturistico2);
+
+    //aqui eu faço print das respostas da carta1 
+    printf("O estado é: %c\n", estado);
+    printf("O codigo da carta é: %c%c\n", codigo1, codigo2);
+    printf("O nome da cidade é: %s\n", cidade);
+    printf("A area da cidade é: %f\n", area);
+    printf("O pibe da cidade é: %f\n", pib);
+    printf("O numero de pontostos turisticos é: %d\n", pontoturistico);
 
     //agora o print da segunda carta para melhor diferenciar, coloque valores diferentes ao digitar
     printf(" O estado é: %c\n", estado2);
