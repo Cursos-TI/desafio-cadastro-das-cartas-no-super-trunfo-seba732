068ff2af55d18cf8a2511ce25620bcdfc9de946b
seba732 <sebajcp@gmail.com>
2025-05-18 18:50:59 +0000

    CartasSuperTrunfo Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ int main() {
     int pontoturistico = 20; // numero inteiro 
     float densidade; // A densidade é calculada dividindo o número total de habitantes pela área da cidade
     float pibpercapita; // PIB per Capita é calculada dividindo o número total do PIB / habitantes
+    double superPoder; // Pontuação total calculada da primeira carta 
 
     //Agora vou declarar as variaveis da carta 2
     char estado2 = 'b'; // código do estado da carta 2
@@ -22,7 +23,7 @@ int main() {
     int pontoturistico2 = 20; // pontos turísticos
     float densidade2; // A densidade é calculada dividindo o número total de habitantes pela área da cidade
     float pibpercapita2; // PIB per Capita é calculada dividindo o número total do PIB / habitantes
-
+    double superPoder2; // Pontuação total calculada da carta 2
 
     //aqui peço para digitar os dados da carta 1
     printf("Digite o codigo do estado de A-H: "); 
@@ -48,8 +49,11 @@ int main() {
 
     densidade = (float) populacao / area; // O cast (float) garante que a divisão seja feita com casas decimais exata
     pibpercapita = (float) (pib * 1e9) / populacao; // O PIB foi fornecido em bilhões, Essa foi a forma que encontre para converter em reais.
-        
     
+    //Superpoder é a soma de todos os critérios da carta. Quanto menor a densidade, maior o valor do inverso.
+    superPoder = pib + (float) populacao + area + (float)pontoturistico + pibpercapita + (1.0f/ densidade);
+
+       
     //Peço para digitar os dados da carta 2, p
     printf("Digite o estado da carta 2 de A-H "); //Peço que digite valores diferentes da primeira 
     scanf("%s", &estado2);
@@ -75,6 +79,8 @@ int main() {
     densidade2 = (float) populacao2 / area2; // O cast (float) garante que a divisão seja feita com casas decimais exata
     pibpercapita2 =  (float) (pib2 * 1e9) / populacao2; // O PIB foi fornecido em bilhões, Essa foi a forma que encontrei para converter em reais.
 
+    // Superpoder é a soma de todos os critérios da carta. Quanto menor a densidade, maior o valor do inverso
+    superPoder2 = pib2 + (float) populacao2 + area2 + (float)pontoturistico2 + pibpercapita2 + (1.0f/ densidade2);
 
     //aqui eu faço print das respostas da carta1 
     printf("O estado é: %c\n", estado); //exibe a letra do estado 
@@ -99,6 +105,28 @@ int main() {
     printf("A densidade é: %.2f hab/km²\n" , densidade2 ); //exibe o cauculo da densidade 
     printf("O PIB per capita é: %.2f reais\n" , pibpercapita2 ); //exibe cauculo do pib per capita
 
+    //comparação das cartas 
+    printf("A popução da primeira carta (%d) e maior que a segunta carta (%d): %d\n",populacao, populacao2, populacao > populacao2);
+    printf("A area da carta primeira carta (%.2f) é maior que a segunda carta (%.2f): %d\n", area, area2, area > area2);
+    printf("O pib da primerira carta (%.2f) é maior que o pibe da segunda carta (%2f): %d\n", pib, pib2, pib > pib2);
+    printf("A primeira carta (%d) tem mais pontos turisticos que a segunda carta (%.d): %d\n", pontoturistico, pontoturistico2, pontoturistico > pontoturistico2);
+    printf("A densidade da primeira (%.2f) carta é maio que a segunda carta (%.2f) %d\n", densidade, densidade2, densidade > densidade2);
+    printf("O PIB per capita da primeira carta (%.2f) e maior que a segunda (%.2f) %d\n", pibpercapita, pibpercapita2, pibpercapita > pibpercapita2);
+   
+         
+
+    // Exibe o valor total de cada carta  
+    printf("Super Poder da carta 1: %.2lf\n", superPoder);
+    printf("Super Poder da carta 2: %.2lf\n", superPoder2);
+
+    //exibe 1 se for verdadeiro e 0 se for falso 
+    printf("População a carta 1 venceu: %d\n", populacao > populacao2); //se respota 1 verdadeiro se 0 falso 
+    printf("Area a carta 1 venceu: %d\n", area > area2); //se respota 1 verdadeiro se 0 falso
+    printf("PIB a carta 1 venceu: %d\n", pib > pib2); //se respota 1 verdadeiro se 0 falso
+    printf("Pontos turisticos a carta 1 venceu: %d\n", pontoturistico > pontoturistico2); //se respota 1 verdadeiro se 0 falso
+    printf("Densidade a carta 1 venceu: %d\n", densidade > densidade2);//se respota 1 verdadeiro se 0 falso
+    printf("PIB per capita a carta 1 venceu: %d\n", pibpercapita > pibpercapita2);//se respota 1 verdadeiro se 0 falso
+    printf("super poder a carta 1 venceu: %d\n", superPoder > superPoder2); //se respota 1 verdadeiro se 0 falso
 
 
     return 0; //finaliza o programa 
